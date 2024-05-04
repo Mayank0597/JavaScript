a6b200f22e51598ef2c20e463ff66915782ef501
Mayank Agrawal <137421758+Mayank0597@users.noreply.github.com>
2024-05-04 12:48:21 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -47,3 +47,41 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);//33
 console.log(typeof stringNumber);// string
+
+//*******************************  Operations  **********************************
+
+let value1 = 3
+let negValue = -value1
+console.log(negValue);//-3
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); // power
+console.log(2/2);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+ 
+console.log(true);//true
+console.log(+true);//1
+//console.log(true+);//error
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2// not preferable
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);//101
+
+
+
