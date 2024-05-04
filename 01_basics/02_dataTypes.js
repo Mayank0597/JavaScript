4972d899aa37e317e629c1d2800468e271f9bd3d
Mayank Agrawal <137421758+Mayank0597@users.noreply.github.com>
2024-05-04 10:25:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3 ) // we are using node.js not browser
+
+//console.log(3+3) console.log("Mayank"); // error
+
+/*
+console.log(3+3)
+console.log("Mayank");
+Write in this way
+*/
+
+let name = "mayank"
+let age =18
+let isLoggedIn = false
+
+/*
+PRIMITIVE DATATYPES
+
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => no value assigned
+symbol => unique
+
+OBJECTS
+
+
+*/
+console.log(typeof "mayank"); // output => string
+console.log(typeof null); // o/p => object, null is of type object, interview question
+console.log(typeof undefined); // o/p => undefined
+
+
+
