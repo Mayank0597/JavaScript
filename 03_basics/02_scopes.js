f8ec10514c438f71ccf2dac0771c9c3ad2c2d14d
Mayank Agrawal <137421758+Mayank0597@users.noreply.github.com>
2024-05-08 07:50:38 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+
+
+// if(true){
+//     let a=10;
+//     const b=20
+//     var c=30
+// }
+
+//console.log(a); => This will show error
+//console.log(b); => This will show error
+//console.log(c); // This will not show error
+
+let a=300
+if(true){
+    let a=100
+    console.log(a);// 100
+}
+console.log(a);// 300
+
