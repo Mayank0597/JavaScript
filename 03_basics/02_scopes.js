e21a443111238dc686fda746624a71f7ec2d8dbf
Mayank Agrawal <137421758+Mayank0597@users.noreply.github.com>
2024-05-08 08:09:18 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,10 +10,50 @@
 //console.log(b); => This will show error
 //console.log(c); // This will not show error
 
-let a=300
-if(true){
-    let a=100
-    console.log(a);// 100
-}
-console.log(a);// 300
+// let a=300
+// if(true){
+//     let a=100
+//     console.log(a);// 100
+// }
+// console.log(a);// 300
 
+
+// function one(){
+//     const userName = "Mayank"
+
+//     function two(){
+//         const website = "Youtube"
+//         console.log(userName);
+//     }
+//     //console.log(website); // error
+
+//     two()
+// }
+
+// one()
+
+// ************************  interesting *************************
+
+// function addOne(num){
+// return num+1
+// }
+// addOne(5)
+    
+// const addTwo = function(num){
+//     return num+2
+// }
+
+// addTwo(5)
+
+
+// console.log(addOne(5)) // no error
+// function addOne(num){
+//     return num+1
+//     }
+
+    // addTwo(5) // error
+    // const addTwo = function(num){
+    //          return num+2
+    //      }
+        
+         
